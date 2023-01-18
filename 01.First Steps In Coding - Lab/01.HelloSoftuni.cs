7c1a177fdf1a810d28d8b9ef077106fd49f28d6f
Nevena Firkova <118193964+nevenafirkova@users.noreply.github.com>
2023-01-18 13:12:16 +0200

    Add files via upload

---
+++ 01.First Steps In Coding - Lab/01.HelloSoftuni.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.HelloSoftuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
---
+++ 01.First Steps In Coding - Lab/05.GreetingByName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.GreetingByName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ozeleneni = double.Parse(Console.ReadLine());
+
+            double kvMetriCena = ozeleneni * 7.61;
+            double namalenie = 0.18 * kvMetriCena;
+            double krainaCena = kvMetriCena - namalenie;
+
+            Console.WriteLine($"The final price is: {krainaCena} lv.");
+            Console.WriteLine($"The discount is: {namalenie} lv.");
+        }
+    }
+}
