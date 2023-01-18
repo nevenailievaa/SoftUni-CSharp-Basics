bc5be0ab75ba095380b9eda66606ba4e5e09597f
Nevena Firkova <118193964+nevenafirkova@users.noreply.github.com>
2023-01-18 13:28:26 +0200

    Add files via upload

---
+++ 03.Conditional Statements - Lab/03.EvenorOdd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03.EvenorOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
---
+++ 03.Conditional Statements - Lab/07.AreaofFigures.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Dynamic;
+
+namespace _07.AreaofFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = (Console.ReadLine());
+
+            if (type == "square")
+            {
+                double sideLenght = double.Parse(Console.ReadLine());
+                double roundResult = sideLenght * sideLenght;
+                Console.WriteLine($"{roundResult:f3}");
+            }
+            if (type == "rectangle")
+            {
+                double sideLength1 = double.Parse(Console.ReadLine());
+                double sideLength2 = double.Parse(Console.ReadLine());
+                double roundResult = sideLength1 * sideLength2;
+                Console.WriteLine($"{roundResult:f3}");
+            }
+            if (type == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double roundResult = Math.PI * radius * radius;
+                Console.WriteLine($"{roundResult:f3}");
+            }
+            if (type == "triangle")
+            {
+                double sideLength = double.Parse(Console.ReadLine());
+                double lenght = double.Parse(Console.ReadLine());
+                double roundResult = (sideLength * lenght) / 2;
+                Console.WriteLine($"{roundResult:f3}");
+            }
+        }
+    }
+}
