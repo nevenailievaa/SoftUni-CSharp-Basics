dca2105f14d3b2ef7a72e5f358c22a48db17cb6c
Nevena Firkova <118193964+nevenafirkova@users.noreply.github.com>
2023-01-18 13:17:29 +0200

    Add files via upload

---
+++ 01.First Steps In Coding - Lab/07.ProjectsCreation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05.GreetingByName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string architect = (Console.ReadLine());
+            int projects = int.Parse(Console.ReadLine());
+            int hours = projects * 3;
+
+            Console.WriteLine($"The architect {architect} will need {hours} hours to complete {projects} project/s.");
+        }
+    }
+}
---
+++ 01.First Steps In Coding - Lab/08.PetShop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05.GreetingByName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int cats = int.Parse(Console.ReadLine());
+
+            double dogs2 = dogs * 2.5;
+            double cats2 = cats * 4;
+
+            double dogsAndCats = dogs2 + cats2;
+
+            Console.WriteLine($"{dogsAndCats} lv.");
+        }
+    }
+}
